6d1ed80e2e2278f1769590e347fcab6abe02c7e6
pytree-Chiayi-Desktop-WSL2 <houjunqimail@gmail.com>
2024-03-01 20:41:28 +0800

    1.23.cpp complete with ChatGPT，順便理解了 reference 和 pointer 的差別

--- Ch1/1.23.cpp
+++ Ch1/1.23.cpp
@@ -1,21 +1,35 @@
 // 寫一個程式，讀取數筆交易記錄，並計數每個 ISBN 出現幾次。
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#include <map>
 #include "../header_files/Sales_item.h"
-
-// 尋找特定的 ISBN
-
 
 int main()
 {
-    std::vector<Sales_item> vec;
+    std::map<std::string, int> isbn_count;
     Sales_item in;
-    if (std::cin >> in)
+
+    if (std::cin >> in) // eg: "0-201-70353-X 4 24.99"
     {
-        vec.push_back(in);
+        isbn_count[in.isbn()] = 1;
         while (std::cin >> in)
         {
+            auto iter = isbn_count.find(in.isbn());
+            if (iter != isbn_count.end())
+            {
+                iter->second++; // 如果此 ISBN 已存在，加 1
+            }
+            else
+            {
+                isbn_count[in.isbn()] = 1;
+            }
         }
+    }
+
+    // output
+    for (const auto &pair : isbn_count)
+    {
+        std::cout << "ISBN: " << pair.first << ", Count: " << pair.second << std::endl;
     }
     return 0;
 }
