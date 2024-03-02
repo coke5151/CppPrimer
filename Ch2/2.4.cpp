89ea383c40010af14820c94107c4c538411eb092
pytree-Chiayi-Desktop-WSL2 <houjunqimail@gmail.com>
2024-03-02 16:38:14 +0800

    0302 14:38 read ch2

---
+++ Ch2/2.4.cpp
@@ -0,0 +1,15 @@
+// 練習 2.4：寫程式來驗證我們對於 2.3.md 的預測
+#include <iostream>
+
+int main()
+{
+    unsigned u = 10, u2 = 42;
+    std::cout << u2 - u << std::endl;
+    std::cout << u - u2 << std::endl;
+    int i = 10, i2 = 42;
+    std::cout << i2 - i << std::endl;
+    std::cout << i - i2 << std::endl;
+    std::cout << i - u << std::endl;
+    std::cout << u - i << std::endl;
+    return 0;
+}
---
+++ Ch2/2.8.cpp
@@ -0,0 +1,10 @@
+// 使用轉義序列，寫出一個程式印出 2M 後面接著一個 newline。
+// 修改這個程式，印出 2，然後一個 tab，然後一個 M，後面接著一個 newline
+#include <iostream>
+
+int main()
+{
+    std::cout << "2M\n";
+    std::cout << "2\tM\n";
+    return 0;
+}
